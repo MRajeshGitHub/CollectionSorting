256551044b9cf538e5de8bf3127ea0271e6db821
ADMIN <ADMIN@DESKTOP-BFTQ2K6>
2024-02-16 00:40:27 +0530

    BT-3657-ArrayList add to another List passing reference vlues

---
+++ src/com/vs/collections/ArrayListConstructors.java
@@ -0,0 +1,19 @@
+package com.vs.collections;
+
+import java.util.ArrayList;
+
+public class ArrayListConstructors {
+
+	public static void main(String[] args) {
+		System.out.println("array list consturctors");
+		
+		ArrayList<Integer>al= new ArrayList<Integer>();
+		al.add(10);
+		al.add(20);
+		al.add(30);
+		ArrayList<Integer> al1= new ArrayList<>(al);//adding list to another list 
+		al1.add(40);
+		al1.add(50);
+		System.out.println(al1);
+	}
+}
