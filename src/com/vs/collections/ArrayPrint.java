bdff122b69424f327376b2accfd1ebed7b1bd9e6
ADMIN <ADMIN@DESKTOP-BFTQ2K6>
2024-02-15 23:21:28 +0530

    BT-1478-Array Print with default values

---
+++ src/com/vs/collections/ArrayPrint.java
@@ -0,0 +1,15 @@
+package com.vs.collections;
+
+public class ArrayPrint {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("array print ");
+		int arr[]= new int[5];
+		System.out.println(arr);//will give hash code because arr is object in java
+		for(int a:arr) {
+			System.out.println(a);
+		}
+	}
+
+}
